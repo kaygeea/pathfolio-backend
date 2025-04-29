70b86d3469eaa075ef2495c8d740eb2ab9328121
Koyejo Adinlewa <adinlewakoyejo@yahoo.com>
2025-04-29 15:47:47 +0100

    chore(debug): add console log on server start

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
+  console.log(
+    `App is listening of port ${process.env.PORT ? process.env.PORT : 3000}`,
+  );
 }
-bootstrap();
+void bootstrap();
